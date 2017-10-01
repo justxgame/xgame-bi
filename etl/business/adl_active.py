4ea30dc0d21b80cfe235be669fab01b7ccf0fa14
zhucw <zhucw@kingnet.com>
2017-10-02 00:09:21 +0800

    fix

--- etl/business/adl_active.py
+++ etl/business/adl_active.py
@@ -41,10 +41,10 @@ formatSql1 = sql1.format(startTimeStr,endTimeStr)
 print 'active sql1=%s' %formatSql1
 
 sql2 = "replace into bi_kpi.new_active (date,server_id,active_num) " \
-       "select tmp.date,tmp.server_id,IFNULL(result.active_num,0) as pay from bi.bdl_mid_tmp tmp left join " \
+       "select tmp.date,tmp.server_id,IFNULL(result.active_num,0) as active_num from bi.bdl_mid_tmp tmp left join " \
        "(select  DATE_FORMAT(login_time,'%Y-%m-%d') as date,  server_id , count(*) as active_num from" \
        " ( select server_id , player_id , min(login_time) as login_time " \
-       " from bi.bdl_login where login_time >= STR_TO_DATE('{0}', '%Y-%m-%d') and login_time  <= STR_TO_DATE('{1}', '%Y-%m-%d') " \
+       " from bi.bdl_login where login_time  <= STR_TO_DATE('{1}', '%Y-%m-%d') " \
        " group by player_id ,server_id) as active_all_table  group by date , server_id) result on tmp.date = result.date and tmp.server_id = result.server_id"
 
 formatSql2 = sql2.format(startTimeStr,endTimeStr)
